85d4deed931535bac8711329a8271bfa7eb2c8a2
John Yung <johnnyyungy@gmail.com>
2025-03-06 07:20:51 +0000

    updates

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -21,8 +21,14 @@ const Interests = () => {
         <section className="interests" id="interests">
             <div className="wrapper">
                 <div>
+                <div>
                     <h2>Outside Interests</h2>
                     <p>Outside of work, my hobbies include traveling, cooking, fitness, and learning different languages.</p>
+                    <p>For fitness, I train Olympic weightlifting and CrossFit. I practice Mixed Martial Arts, in particular Muay Thai and Brazilian Jiu-Jitsu.</p>
+                    <p>I like to collect limited edition action figures, such as Transformers and Power Rangers.</p>
+                    <p>I am also interested in photography and videography. I combine my interests by taking photos on my travels and making videos for my YouTube cooking channel, <a href="https://www.youtube.com/channel/UCZhsbHXGqVWHivekOiC9ofQ" target="_blank" rel="noopener noreferrer">Hello! Taste</a>.</p>
+                    <p>I also have a fitness blog that is built in WordPress: <a href="http://www.trainsmartnothard.com/" target="_blank" rel="noopener noreferrer">www.trainsmartnothard.com</a></p>
+                </div>
                 </div>
                 <div className="photo">
                     <div className="slideshow-container">
